bf79a4327cf5e8620d65d8cb7830723852742570
colin <rongbo17173@qq.com>
2026-01-20 13:26:42 +0800

    1

--- Assets/Game/Scripts/Hotfix/Config/UIConfig.cs
+++ Assets/Game/Scripts/Hotfix/Config/UIConfig.cs
@@ -1,14 +1,13 @@
 public static class UIConfig
 {
     public static string Login = "Prefabs/UI/Login/UILogin";
-    public static string CreateScene = "UICreateScene";
+    public static string CreateScene = "Prefabs/UI/Scene/UICreateScene";
     public static string Main = "Prefabs/UI/Main/UIMain";
-    public static string Friend = "UIFriend";
-    public static string Bag = "UIBag";
-    public static string Chat = "UIChat";
-
-    public static string Confirm = "UIConfirm";
-    public static string Setting = "UISetting";
-    public static string Skill = "UISkill";
-    public static string UserInfo = "UIUserInfo";
+    public static string Friend = "Prefabs/UI/Friend/UIFriend";
+    public static string Bag = "Prefabs/UI/Bag/UIBag";
+    public static string Chat = "Prefabs/UI/Chat/UIChat";
+    public static string Confirm = "Prefabs/UI/Confirm/UIConfirm";
+    public static string Setting = "Prefabs/UI/Setting/UISetting";
+    public static string Skill = "Prefabs/UI/Skill/UISkill";
+    public static string UserInfo = "Prefabs/UI/UserInfo/UIUserInfo";
 }
